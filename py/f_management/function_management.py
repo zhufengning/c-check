94a97259f3ba6ca749f64e49ba91c1a92e0d4071
muyu88 <muyu1522021@163.com>
2024-07-15 23:47:19 +0800

    饼图

--- py/f_management/function_management.py
+++ py/f_management/function_management.py
@@ -26,7 +26,44 @@ def add_function(folder_path, file_name, key,fun_name,fun_level,fun_solution)->b
         functions.append({"fun_name":fun_name,"fun_level":fun_level,"fun_solution":fun_solution})
         encrypt_data(folder_path,file_name,key,json.dumps(functions))
         return True
+def initialize_functions(folder_path, file_name, key):
+    """向用户文件添加内置危险函数
 
+    :param folder_path: _description_
+    :param file_name: _description_
+    :param key: _description_
+    """
+    functions=[
+    {'fun_name': 'gets', 'fun_level': '最危险', 'fun_solution': '使用 fgets（buf, size, stdin）这几乎总是一个大问题'},
+    {'fun_name': 'strcpy', 'fun_level': '很危险', 'fun_solution': '改为使用 strncpy'},
+    {'fun_name': 'strcat', 'fun_level': '很危险', 'fun_solution': '改为使用 strncat'},
+    {'fun_name': 'sprintf', 'fun_level': '很危险', 'fun_solution': '改为使用 snprintf，或者使用精度说明符'},
+    {'fun_name': 'scanf', 'fun_level': '很危险', 'fun_solution': '使用精度说明符，或自己进行解析'},
+    {'fun_name': 'sscanf', 'fun_level': '很危险', 'fun_solution': '使用精度说明符，或自己进行解析'},
+    {'fun_name': 'vsprintf', 'fun_level': '很危险', 'fun_solution': '改为使用 vsnprintf，或者使用精度说明符'},
+    {'fun_name': 'vscanf', 'fun_level': '很危险', 'fun_solution': '使用精度说明符，或自己进行解析'},
+    {'fun_name': 'vsscanf', 'fun_level': '很危险', 'fun_solution': '使用精度说明符，或自己进行解析'},
+    {'fun_name': 'streadd', 'fun_level': '很危险', 'fun_solution': '确保分配的目的地参数大小是源参数大小的四倍'},
+    {'fun_name': 'strecpy', 'fun_level': '很危险', 'fun_solution': '确保分配的目的地参数大小是源参数大小的四倍'},
+    {'fun_name': 'strtrns', 'fun_level': '危险', 'fun_solution': '手工检查来查看目的地大小是否至少与源字符串相等'},
+    {'fun_name': 'realpath', 'fun_level': '稍低危险', 'fun_solution': '分配缓冲区大小为 MAXPATHLEN。同样，手工检查参数以确保输入参数不超过 MAXPATHLEN'},
+    {'fun_name': 'syslog', 'fun_level': '稍低危险', 'fun_solution': '在将字符串输入传递给该函数之前，将所有字符串输入截成合理的大小'},
+    {'fun_name': 'getopt_long', 'fun_level': '稍低危险', 'fun_solution': '在将字符串输入传递给该函数之前，将所有字符串输入截成合理的大小'},
+     {'fun_name': 'getopt', 'fun_level': '稍低危险', 'fun_solution': '在将字符串输入传递给该函数之前，将所有字符串输入截成合理的大小'},
+    {'fun_name': 'getpass', 'fun_level': '稍低危险', 'fun_solution': '在将字符串输入传递给该函数之前，将所有字符串输入截成合理的大小'},
+    {'fun_name': 'getchar', 'fun_level': '中等危险', 'fun_solution': '如果在循环中使用该函数，确保检查缓冲区边界'},
+    {'fun_name': 'fgetc', 'fun_level': '中等危险', 'fun_solution': '如果在循环中使用该函数，确保检查缓冲区边界'},
+    {'fun_name': 'getc', 'fun_level': '中等危险', 'fun_solution': '如果在循环中使用该函数，确保检查缓冲区边界'},
+    {'fun_name': 'read', 'fun_level': '中等危险', 'fun_solution': '如果在循环中使用该函数，确保检查缓冲区边界'},
+    {'fun_name': 'bcopy', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'},
+    {'fun_name': 'fgets', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'},
+    {'fun_name': 'memcpy', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'},
+    {'fun_name': 'strccpy', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'},
+    {'fun_name': 'strncpy', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'},
+    {'fun_name': 'vsnprintf', 'fun_level': '低危险', 'fun_solution': '确保缓冲区大小与它所说的一样大'}
+]
+    encrypt_data(folder_path,file_name,key,json.dumps(functions))
+    
 def del_function(folder_path, file_name, key,fun_name)->bool:
     """删除一条危险函数，成功返回true,失败返回false
 
@@ -120,12 +157,19 @@ if __name__ == "__main__":
     # print(add_function("usera","函数表", "key","gets","最危险","使用 fgets（buf, size, stdin）。这几乎总是一个大问题！"))
     # add_function("usera","函数表", "key","strcpy","很危险","改为使用 strncpy。")
     # add_function("usera","函数表", "key","function1","很危险","改为使用 strncpy。")
-    add_function("usera","函数表", "key","f","很危险","不能使用。")
+    # add_function("usera","函数表", "key","f","很危险","不能使用。")
     # del_function("usera","函数表", "key","get")
     # print(json.loads(decrypt_data("usera","函数表", "key")))
     # print(find_function("usera","函数表", "key","strcpy"))
     # add_function("usera","函数表", "key","1111","很危险","改为使用 strncpy。")
-    print(show_functions("usera","函数表", "key"))
+    # print(show_functions("usera","函数表", "key"))
     # print(filter_by_level(show_functions("usera","函数表", "key"),"很危险"))
     # print(search_by_name_regex(show_functions("usera","函数表", "key"),'\w*ets'))
+    initialize_functions("usera","初始化表", "key")
+    i=0
+    for entry in show_functions("usera","初始化表", "key"):
+        i+=1
+        print(entry)
+    print(i)
+
 
--- py/g_report/generate_report.py
+++ py/g_report/generate_report.py
@@ -1,15 +1,23 @@
-import os,sys
+import os,sys,tempfile,io
+
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from datetime import datetime
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle
 from reportlab.platypus import Paragraph, SimpleDocTemplate,Image
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.graphics import renderPM
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.renderPDF import drawToString
+from PIL import Image as PILImage
 from c_parser.DFSVisitor import DFSVisitor
 import c_parser.ply.yacc as yacc
 from c_parser import AST, cparser
 from f_management import function_management
+import matplotlib.pyplot as plt
 
 
 class Risk_fun_Visitor(DFSVisitor):
@@ -33,8 +41,42 @@ class Risk_fun_Visitor(DFSVisitor):
                 self.risk_fun_nodes.append(node)
                 self.risk_fun_infos.append({"position":node.pos,"fun_name":entry["fun_name"],"fun_level":entry["fun_level"],"fun_solution":entry["fun_solution"]})
 
-def generate_pie_chart()
-def generate_report(file_name)->str:
+def generate_risk_fun_pie_chart(risk_fun_infos:list,level_nums:dict):
+    """生成风险函数饼图
+    Args:
+        risk_fun_infos (list): 风险函数信息列表
+    """
+     # 提取标签和大小
+    labels = list(level_nums.keys())
+    sizes = list(level_nums.values())
+    
+    # 设置中文显示
+    plt.rcParams['font.sans-serif'] = 'SimHei'
+    
+    # 创建画布
+    plt.figure(figsize=(8, 6))
+    
+    # 绘制饼图
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    plt.title('风险函数分布饼图', fontsize=16)
+    
+    # 添加图例
+    plt.legend(labels, loc="best", fontsize=10)
+    
+    # 保持图像比例一致
+    plt.axis('equal')
+    
+    # 保存饼图到临时文件
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
+        chart_filename = temp_file.name
+        # chart_filename="py\g_report\图片"
+        plt.savefig(chart_filename)
+    
+    plt.close()
+    print(chart_filename)
+    return chart_filename
+        
+def generate_report(file_name:str,program:str,risk_fun_infos:list)->str:
     """生成报告
 
     :param file_name: _description_
@@ -50,24 +92,48 @@ def generate_report(file_name)->str:
     pdfmetrics.registerFont(TTFont('SimSun', 'py\g_report\SimSun.ttf'))  #注册字体
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(fontName='SimSun', name='Song', leading=20, fontSize=12))  # 自己增加新注册的字体
+    styles['Title'].fontName = 'SimSun' #将标题字体更改为'SimSun'
     # 创建 SimpleDocTemplate 对象时传入完整路径
     pdf = SimpleDocTemplate(full_path, pagesize=letter)
+    # 获取当前日期的字符串格式
+    current_date_str = datetime.now().strftime('%Y-%m-%d')
     content = [
-        Paragraph("Hello, World!", styles['Title']),
-        Paragraph("项目名称",styles['Song']),
-        Paragraph("审计日期",styles['Song']),
-        Paragraph("This is a simple PDF document generated using ReportLab.", styles['Normal']),
+        Paragraph("审计报告", styles['Title']),
+        Paragraph("项目名称："+program,styles['Song']),
+        Paragraph("审计日期："+current_date_str,styles['Song']),
         # 可以添加更多的 Paragraph 或者其他 ReportLab 组件
     ]
-    content.append(Image("py\g_report\风险报告.jpg",width=500,height=200))
-
+    # level_nums = {
+    #     '最危险': 0,
+    #     '很危险': 0,
+    #     '危险': 0,
+    #     '稍低危险': 0,
+    #     '中等危险': 0,
+    #     '低危险': 0,
+    #     # '无效函数': 0
+    # }
+    # for func in risk_fun_infos:
+    #     level = func['fun_level']
+    #     if level in level_nums:
+    #         level_nums[level] += 1
+    level_nums = {
+        '最危险': 1,
+        '很危险': 2,
+        '危险': 3,
+        '稍低危险': 4,
+        '中等危险': 5,
+        '低危险': 6,
+        # '无效函数': 0
+    }
+    pie_chart_path = generate_risk_fun_pie_chart(risk_fun_infos,level_nums)
+    content.append(Image(pie_chart_path, width=400, height=400,hAlign='CENTER', kind='proportional'))
     # 将内容添加到文档中
     pdf.build(content)
+    # os.remove(pie_chart_path)
 
 
 if __name__ == '__main__':
-    file_name = "report.pdf"
-    generate_report(file_name)
+    
     cparser = cparser.Cparser()
     parser = yacc.yacc(module=cparser)
     source2 = """#include <stdio.h>
@@ -102,9 +168,11 @@ if __name__ == '__main__':
     risk_functions=[{'fun_name': 'gets', 'fun_level': '最危险', 'fun_solution': '使用 fgets（buf, size, stdin）。这几乎总是一个大问题！'},
                      {'fun_name': 'strcpy', 'fun_level': '很危险', 'fun_solution': '改为使用 strncpy。'}, 
                      {'fun_name': 'function1', 'fun_level': '很危险', 'fun_solution': '改为使用 strncpy。'}, 
-                     {'fun_name': 'f', 'fun_level': '很 危险', 'fun_solution': '不能使用。'}]
+                     {'fun_name': 'f', 'fun_level': '很危险', 'fun_solution': '不能使用。'}]
     v=Risk_fun_Visitor(risk_functions)
     v.visit(ast)
     print(v.risk_fun_infos)
     for node in v.risk_fun_nodes:
         print(node)
+    file_name = "report.pdf"
+    generate_report(file_name,"test",v.risk_fun_infos)
